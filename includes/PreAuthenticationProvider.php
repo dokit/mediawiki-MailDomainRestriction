1f60572bb514888d3590b6edaddfc01b6c6936b7
Pierre Boutet <boutetpierre@yahoo.fr>
2019-11-13 16:45:11 +0100

    add phpcs

--- includes/PreAuthenticationProvider.php
+++ includes/PreAuthenticationProvider.php
@@ -1,48 +1,55 @@
 <?php
-
 namespace MailDomainRestriction;
 
+/**
+ * Class PreAuthenticationProvider to check domain of email before authenticate
+ */
+
 use MediaWiki\Auth\AbstractPreAuthenticationProvider;
-use MediaWiki\Auth\AuthenticationRequest;
-use MediaWiki\Auth\AuthenticationResponse;
-use MediaWiki\Auth\AuthManager;
-use MediaWiki\Logger\LoggerFactory;
 use Status;
 
 class PreAuthenticationProvider extends AbstractPreAuthenticationProvider {
 
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \MediaWiki\Auth\AbstractPreAuthenticationProvider::testForAccountCreation()
+	 *
+	 * @param \User $user
+	 * @param \User $creator
+	 * @param array $reqs
+	 * @return Status
+	 */
 	public function testForAccountCreation( $user, $creator, array $reqs ) {
 		global $wgMailDomainRestrictionBlacklist, $wgMailDomainRestrictionWhitelist;
 
-
-		if ( ! $wgMailDomainRestrictionBlacklist && ! $wgMailDomainRestrictionWhitelist ) {
+		if ( !$wgMailDomainRestrictionBlacklist && !$wgMailDomainRestrictionWhitelist ) {
 			return Status::newGood();
 		}
 
 		// extract domain from email :
 		$email = $user->getEmail();
-		if (strpos($email, '@') === false) {
-			return $this->makeError( wfMessage('maildomainrestriction-mailinvalid') );
+		if ( strpos( $email, '@' ) === false ) {
+			return $this->makeError( wfMessage( 'maildomainrestriction-mailinvalid' ) );
 		}
-		$domain = substr($email, strpos($email, '@') + 1 );
+		$domain = substr( $email, strpos( $email, '@' ) + 1 );
 
 		// check against blacklist :
-		if ($wgMailDomainRestrictionBlacklist) {
-			if ( in_array($domain, $wgMailDomainRestrictionBlacklist) ) {
-				return $this->makeError( wfMessage('maildomainrestriction-mailisBlacklisted') );
+		if ( $wgMailDomainRestrictionBlacklist ) {
+			if ( in_array( $domain, $wgMailDomainRestrictionBlacklist ) ) {
+				return $this->makeError( wfMessage( 'maildomainrestriction-mailisBlacklisted' ) );
 			}
 		}
 
 		// check against whitelist :
-		if ($wgMailDomainRestrictionWhitelist) {
-			if ( ! in_array($domain, $wgMailDomainRestrictionWhitelist) ) {
-				return $this->makeError(wfMessage('maildomainrestriction-mailisnotallowed') );
+		if ( $wgMailDomainRestrictionWhitelist ) {
+			if ( !in_array( $domain, $wgMailDomainRestrictionWhitelist ) ) {
+				return $this->makeError( wfMessage( 'maildomainrestriction-mailisnotallowed' ) );
 			}
 		}
 
 		return \Status::newGood();
 	}
-
 
 	/**
 	 * @param string $message Message
@@ -50,12 +57,12 @@ class PreAuthenticationProvider extends AbstractPreAuthenticationProvider {
 	 */
 	protected function makeError( $message ) {
 		global $wgMailDomainRestrictionMessage, $wgMailDomainRestrictionWhitelist;
-		if ($wgMailDomainRestrictionMessage) {
+		if ( $wgMailDomainRestrictionMessage ) {
 			$message = $wgMailDomainRestrictionMessage;
 		}
-		if ($wgMailDomainRestrictionWhitelist && count($wgMailDomainRestrictionWhitelist) == 1) {
-			$allowedDomain = array_pop($wgMailDomainRestrictionWhitelist);
-			$message = wfMessage('maildomainrestriction-error-allowedmail', $allowedDomain);
+		if ( $wgMailDomainRestrictionWhitelist && count( $wgMailDomainRestrictionWhitelist ) == 1 ) {
+			$allowedDomain = array_pop( $wgMailDomainRestrictionWhitelist );
+			$message = wfMessage( 'maildomainrestriction-error-allowedmail', $allowedDomain );
 		}
 		return Status::newFatal( $message );
 	}
